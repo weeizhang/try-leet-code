fc0f6832b58c84ac46e3dec38a858819086b2657
Zhang Wei <weeizhang@outlook.com>
2017-07-04 12:25:37 +0800

    27 Remove Element

---
+++ src/RemoveElement27.java
@@ -0,0 +1,12 @@
+public class RemoveElement27 {
+    public int removeElement(int[] nums, int val) {
+        int length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[length] = nums[i];
+                length++;
+            }
+        }
+        return length;
+    }
+}
