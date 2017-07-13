f9df76f04a3645aeba75d59a8b638795cbd7c342
Zhang Wei <weeizhang@outlook.com>
2017-07-13 13:01:22 +0800

    70 Climbing Stairs

---
+++ src/ClimbingStairs70.java
@@ -0,0 +1,15 @@
+public class ClimbingStairs70 {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int prev1 = 2;
+        int prev2 = 1;
+        for (int i = 3; i <= n; i++) {
+            int cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+}
